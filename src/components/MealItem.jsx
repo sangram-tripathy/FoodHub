79ba0cf3af1a138074e72c445be83dcb9d400cef
Sangram Tripathy <sangramtripathy5610@gmail.com>
2026-02-19 15:43:15 +0530

    Update MealItem.jsx

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
  import { currencyFormatter } from '../util/formatting'
  import Button from './UI/Button'
  import CartContext from '../store/CartContext'
+ import { API_URL } from '../config'
 
 const MealItem = ({meal}) => {
    const cartCtx= useContext(CartContext);
@@ -13,7 +14,7 @@ const MealItem = ({meal}) => {
   return (
     <li className='meal-item'>
         <article>
-            <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+            <img src={`${API_URL}/${meal.image}`} alt={meal.name} />
             <div>
                 <h3>{meal.name}</h3>
                 <p className='meal-item-price'>
